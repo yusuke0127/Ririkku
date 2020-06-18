f3ab2fab96e2325f402f41a9ceb38db53c210517
yusuke ishida <ishidayusuke01@gmail.com>
2020-06-19 00:24:39 +0900

    Add Create song feature

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,4 +7,28 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
     authorize @song
   end
+
+  def new
+    @artist = Artist.find(params[:artist_id])
+    @song = Song.new
+    authorize @song
+  end
+
+  def create
+    @song = Song.new(song_params)
+    @artist = Artist.find(params[:artist_id])
+    @song.artist = @artist
+    authorize @song
+    if @song.save!
+      redirect_to artist_path(@artist)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:en_title, :jp_title, :en_lyric, :jp_lyric, :genre, :composer, :release)
+  end
 end
