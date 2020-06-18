c7f86ad2dc292e29e83dcc0a548dc88f3b54cedb
yusuke ishida <ishidayusuke01@gmail.com>
2020-06-18 17:40:11 +0900

    Added policy for song

--- app/policies/artist_policy.rb
+++ app/policies/artist_policy.rb
@@ -7,7 +7,6 @@ class ArtistPolicy < ApplicationPolicy
 
   def show?
     true
-
   end
 
   def create?
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :artists do
-    resources :songs, only: [ :new, :create ]
+    resources :songs, only: [ :index, :new, :create ]
   end
 end
